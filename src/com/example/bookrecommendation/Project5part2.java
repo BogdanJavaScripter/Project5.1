b2591e68370ecbc592d709cc1b06ac2b825406b7
BogdanJavaScripter <bogdan.koltok@auk.edu.ua>
2023-09-14 23:50:00 +0300

    YEp

--- src/com/example/bookrecommendation/Project5part2.java
+++ src/com/example/bookrecommendation/Project5part2.java
@@ -1,28 +1,33 @@
 package com.example.bookrecommendation;
+
 import java.util.Scanner;
+
+
 public class Project5part2 {
 
-    public class Main {
+    public static class Main {
         public static void main(String[] args) {
-            char userChoice;
+            char user;
 
             Scanner keyboard = new Scanner(System.in);
 
             do {
-                print("Hello If youre here, you might want advice.");
-                print("Im a book guy and can recommend interesting books.");
+                print("Hello If you're here you might want advice");
+                print("Im a book guy and can recommend interesting books");
                 int age = getUserAge(keyboard);
 
                 recommendBookAge(age);
 
-                userChoice = getUserChoice(keyboard);
-            } while (userChoice == 'Y' || userChoice == 'y');
+                user = getUserChoice(keyboard);
+            } while (user == 'Y' || user == 'y');
         }
+
 
         private static int getUserAge(Scanner keyboard) {
             print("Please enter your age: ");
             return keyboard.nextInt();
         }
+
 
         private static void recommendBookAge(int age) {
             print("Book found:");
@@ -32,20 +37,22 @@ public class Project5part2 {
                 print("Superstrings and the Search for the Theory of Everything 1st Edition.");
             } else if (age >= 14 && age <= 25) {
                 print("Book not found.");
-                print("You're at university and you might not have much time to read Goodbye");
+                print("You're at universit and you might not have much time to read. Goodbye");
             } else if (age >= 26 && age <= 50) {
-                print("That will be such a nice lesson for you Atomic Habits.");
+                print("That will be such a nice lesson for you: Atomic Habits.");
             } else {
-                print("You have already reached the state of bliss and genius.");
-                print("I will tell you some books after I beat my grandfather in chess.");
+                print("You have already reached the state of bliss and genius");
+                print("I will tell you some books after I beat my grandfather in chess");
             }
         }
+
 
         private static char getUserChoice(Scanner keyboard) {
             print("Do you want to continue Y or N ");
             return keyboard.next().charAt(0);
         }
 
+        // Method for printing messages
         private static void print(String message) {
             System.out.println(message);
         }
